f20ab36e7af2d32284efe4201441e67cc63bf72a
jloescher <jonathan@jloescher.com>
2022-12-16 22:44:21 -0500

    Initial commit & defined enemy and hero

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+# Define the hero and the enemy
+hero = {
+    "name": "Hercules",
+    "health": 100,
+    "attack moves": {"Punch": 10, "Kick": 15, "Slap": 5},
+}
+
+enemy = {
+    "name": "Hydra",
+    "health": 50,
+    "attack moves": {"Bite": 10, "Tail Whip": 5, "Roar": 15},
+}
