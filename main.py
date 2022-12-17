c7bd886c76e8fb5adae308601fe486f2a600710b
jloescher <jonathan@jloescher.com>
2022-12-16 22:45:06 -0500

    Defined hero attacks

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ enemy = {
     "health": 50,
     "attack moves": {"Bite": 10, "Tail Whip": 5, "Roar": 15},
 }
+
+# Define the attack moves and values for the hero and the enemy
+hero_attacks = {"Punch": 10, "Kick": 15, "Slap": 5}
+
+enemy_attacks = {"Bite": 10, "Tail Whip": 5, "Roar": 15}
+
